67c86ad89f442fe8cc38dba8c98119caa794b5c2
Sam Potter <sam_pot_97@hotmail.com>
2017-02-19 19:09:07 +0000

    Json handling

---
+++ Back-End/VScript-Core/VScript-Core/Graph/Json/JsonValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VScript_Core.Graph.Json
+{
+	public class JsonValue
+	{
+	}
+
+	public class JsonValue<Type> : JsonValue
+	{
+		public Type raw_value;
+
+		public JsonValue(Type value)
+		{
+			this.raw_value = value;
+		}
+
+		public override string ToString()
+		{
+            return raw_value.ToString();
+		}
+	}
+}
--- Back-End/VScript-Core/VScript-Testing/Program.cs
+++ Back-End/VScript-Core/VScript-Testing/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VScript_Core;
+using VScript_Core.Graph;
+using VScript_Core.Graph.Json;
 
 using VScript_Core.System;
 
@@ -14,10 +16,32 @@ namespace VScript_Testing
 		static void Main(string[] args)
 		{
 			Logger.Log("Hello World");
-			PythonProgram test_python = new PythonProgram("/Intermediate/print_test.py");
-			test_python.Run("");
+			
+			/*
+			JsonObject another_json = new JsonObject();
+			another_json.Put("yes", true);
+
+			JsonObject json = new JsonObject();
+			json.Put("test", 2);
+			json.Put("a", 3);
+			json.Put("b", "awe");
+			json.PutList("test", new List<string>{ "a", "we", "awewa"});
+
+			another_json.Put("json", json);
+            Logger.Log(another_json.ToString());
+			*/
+
+			JsonObject json = new JsonObject("{\"test\":30,\"a\":{\"force\":true},\"arr\":[0,1,2,\"apple\"],\"b\":\"apple\"}");
+			Logger.Log(json.ToString());
+			Logger.Log(json.Get<int>("test").ToString());
+			Logger.Log("force: "+ json.GetObject("a").Get<bool>("force"));
+
+			foreach(int i in json.GetList<int>("arr"))
+				Logger.Log("" + i);
+
+
 			Logger.Log("Done");
-            Console.ReadLine();
+			Console.ReadLine();
 		}
 	}
 }
