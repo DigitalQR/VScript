18c2c06685f9f250a2c6d5abcba4c46733f8b108
Randhir Singh Harra <harrar@uni.coventry.ac.uk>
2017-02-21 20:38:46 +0000

    Blank Node Development

---
+++ Front-End/Assets/Scripts/nodeDrag.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nodeDrag : MonoBehaviour {
+
+    public float speed;
+    public GameObject point;
+
+    private bool move = false;
+    private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+
+            if (move == false)
+                move = true;
+            Instantiate(point, target, Quaternion.identity);
+        }
+
+        if (move == true)
+            transform.position = Vector3.MoveTowards(transform.position, target, speed *Time.deltaTime);
+
+
+
+		
+	}
+}
