ede5b008d6cb972fdd2fba38fdc9699b9d4c1953
Sam Potter <sam_pot_97@hotmail.com>
2017-03-14 21:22:13 +0000

    Console IO

---
+++ Front-End/Assets/Scripts/UI/ConsoleInputListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsoleInputListener : MonoBehaviour {
+
+	[SerializeField]
+	private InputField InputField;
+
+	public void OnEndEdit()
+	{
+		HandleInput(InputField.text);
+		InputField.text = "";
+    }
+
+	void HandleInput(string input)
+	{
+		Debug.Log("Do something with input '" + input + "'");
+
+		//TESTING ONLY
+		ConsoleOutputListener output = FindObjectOfType<ConsoleOutputListener>();
+		output.Print(input);
+
+	}
+}
