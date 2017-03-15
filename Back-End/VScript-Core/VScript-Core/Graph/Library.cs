0c2454739333299a5bbc899f403cddc36dbf4beb
Sam Potter <sam_pot_97@hotmail.com>
2017-03-15 19:08:47 +0000

    Added central library for nodes and modules

---
+++ Back-End/VScript-Core/VScript-Core/Graph/Library.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VScript_Core.Graph
+{
+	public class Library
+	{
+		//Singlton accessing
+		private static Library library_singlton;
+		public static Library main
+		{
+			get
+			{
+				if (library_singlton == null)
+					library_singlton = new Library();
+				return library_singlton;
+            }
+		}
+
+
+		private Dictionary<int, Dictionary<int, Node>> module_nodes = new Dictionary<int, Dictionary<int, Node>>();
+		private Node start_node;
+
+		private Library()
+		{
+			start_node = new Node("Start");
+			start_node.colour_r = 0.1f;
+			start_node.colour_g = 0.1f;
+			start_node.colour_b = 1.0f;
+
+			start_node.source = "{eo:end}";
+
+			IOType output = new IOType();
+			output.name = "end";
+            output.display_name = "";
+			output.is_execution = true;
+
+			start_node.outputs.Add(output);
+		}
+
+		public void LoadModule(string folder, string name)
+		{
+			Module module = new Module(folder + "/" + name, "Python3");
+			module.Import();
+
+			Dictionary<int, Node> nodes = new Dictionary<int, Node>();
+
+			foreach (string node_name in module.nodes)
+			{
+				Node node = new Node(folder + "/" + node_name);
+				node.Import();
+
+				nodes.Add(node.id, node);
+			}
+
+			module_nodes.Add(module.id, nodes);
+        }
+
+		public Node GetNode(int module_id, int node_id)
+		{
+			if (module_id == 0 && node_id == 0)
+				return start_node;
+			
+			return module_nodes[module_id][node_id];
+		}
+
+	}
+}
--- Back-End/VScript-Core/VScript-Testing/Program.cs
+++ Back-End/VScript-Core/VScript-Testing/Program.cs
@@ -16,33 +16,9 @@ namespace VScript_Testing
 		static void Main(string[] args)
 		{
 			Logger.Log("Hello World");
-			/*
-			Graph graph = new Graph("Test");
-			graph.Import();
-			//graph.root_node.forwards_conn.Add(new NodeMeta(3453, 23));
-			//graph.root_node.forwards_conn.Add(new NodeMeta(3453, 33));
-			//graph.root_node.backwards_conn.Add(new NodeMeta(3453, 13));
-			//graph.Export();
 
-			Logger.Log(graph.ToString());
-
-			Module module = new Module("Test");
-			module.Export();
-			Logger.Log(module.ToString());
-			*/
-
-			Node node = new Node("Example");
-			//node.source = "Print(i)";
-			//node.id = 69;
-
-			IOType input_0 = new IOType();
-			//input_0.name = "in 0";
-			//input_0.is_execution = true;
-			//node.inputs.Add(input_0);
-			node.inputs.Add(input_0);
-			node.Export();
-			//node.Import();
-
+			Library.main.LoadModule("../../Modules", "Core");
+			Node node = Library.main.GetNode(1, 2);
 			Logger.Log(node.ToString());
 
 			Logger.Log("Done");
