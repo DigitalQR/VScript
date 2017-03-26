9e4d86319b835c3260579790bb32afdbe3f4db8f
DigitalQR (Laptop) <sam_pot_97@hotmail.com>
2017-03-26 14:55:16 +0100

    Graph entries in select dialog

---
+++ Front-End/Assets/Scripts/Graph/GraphEntryDialog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class GraphEntryDialog : MonoBehaviour {
+
+	[SerializeField]
+	private Text Entry;
+
+	public void SetName(string name)
+	{
+		Entry.text = name;
+	}
+
+	public void Select()
+	{
+		GraphSelectDialog.main.Select(Entry.text);
+	}
+
+}
--- Front-End/Assets/Scripts/Graph/GraphSelectDialog.cs
+++ Front-End/Assets/Scripts/Graph/GraphSelectDialog.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.IO;
 using UnityEngine.UI;
+using VScript_Core;
 
 
 public delegate void GraphOperation(string GraphName);
@@ -14,12 +16,21 @@ public class GraphSelectDialog : MonoBehaviour {
 
 	[SerializeField]
 	private InputField GraphInput;
+	[SerializeField]
+	private GameObject ScrollPane;
+	[SerializeField]
+	private GraphEntryDialog BaseEntry;
 	private GraphOperation CurrentOperation;
 
 	void Start ()
 	{
 		gameObject.SetActive(false);
 		main = this;
+	}
+
+	public void Select(string name)
+	{
+		GraphInput.text = name;
 	}
 
 	public void OnAccept()
@@ -45,7 +56,32 @@ public class GraphSelectDialog : MonoBehaviour {
 		if (operation == null)
 			return;
 
+		RebuildEntries();
 		CurrentOperation = operation;
 		gameObject.SetActive(true);
+	}
+
+	void ClearEntries()
+	{
+		foreach (GraphEntryDialog entry in ScrollPane.GetComponentsInChildren<GraphEntryDialog>())
+			Destroy(entry.gameObject);
+	}
+
+	void RebuildEntries()
+	{
+		ClearEntries();
+		string project_directory = VScriptEngine.engine_directory + VScriptEngine.project_directory + "/";
+
+		foreach (string file in Directory.GetFiles(project_directory))
+		{
+			if (!file.EndsWith(VScript_Core.Graphing.Graph.file_extention))
+				continue;
+
+			string name = file.Substring(project_directory.Length, file.Length - project_directory.Length - VScript_Core.Graphing.Graph.file_extention.Length);
+
+			GraphEntryDialog entry = Instantiate(BaseEntry, ScrollPane.transform);
+			entry.transform.localScale = new Vector3(1, 1, 1);
+			entry.SetName(name);
+		}
 	}
 }
